7f1113e67ad8bb7dcf750b352055d87e4365333a
tanmay45226 <tanmaybari17@gmail.com>
2024-12-11 10:11:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for navigation links
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const target = document.querySelector(e.target.getAttribute('href'));
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
